444cc35e22d8533d45ed532ab9c069bb31a32cab
leafyskull <danielholt505@gmail.com>
2026-08-02 21:30:27 -0500

    Minor bug fixing

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ class TaskList:
 
 # User can add, delete, and view items in a to-do list.
 def main():
-    print("Hello world!")
 
     taskList = TaskList()
     newTaskId = 1
@@ -36,13 +35,13 @@ def main():
                 # 3 = delete a task
 
     while keepGoing:
-        print("********************\n")
-        print("> Options:\n")
-        print("[0] Exit program\n")
-        print("[1] View to-do list\n")
+        print("********************")
+        print("> Options:")
+        print("[0] Exit program")
+        print("[1] View to-do list")
         print("[2] Add new task")
-        print("[3] Delete a task\n")
-        print("********************\n")
+        print("[3] Delete a task")
+        print("********************")
         print("\n")
 
         option = input("Select an option: ")
@@ -50,42 +49,50 @@ def main():
         match option:
 
             # Exit program
-            case 0:
+            case "0":
                 print("Exiting program...")
                 sys.exit(0)
 
             # Print task list
-            case 1:
-                print("* TASK LIST: *\n")
+            case "1":
+                print("* TASK LIST: *")
 
-                for item in taskList:
+                for item in taskList.items:
                     print(f"[{item.id}] {item.description}")
 
+                enterToContinue()
+
             # Add a new task
-            case 2:
+            case "2":
                 newTaskDescription = input("New task description: ")
                 newTask = Task(newTaskDescription, newTaskId)
                 newTaskId += 1
-                taskList.add(newTask)
+                taskList.items.append(newTask)
+
+                enterToContinue()
 
             # Delete a task
-            case 3:
-                print("Which task to delete?\n")
+            case "3":
+                print("Which task to delete?")
                 taskIdToDelete = input("Enter task ID: ")
 
                 taskRemoved = False
-                for task in taskList:
-                    if task.id == taskIdToDelete:
-                        taskList.remove(task)
+
+                for task in taskList.items:
+                    if str(task.id) == taskIdToDelete:
+                        taskList.items.remove(task)
                         print(f"Task: \"{task.description}\" removed from list.")
                         taskRemoved = True
+                        enterToContinue()
 
                 if not taskRemoved:
                     print(f"No task with ID {taskIdToDelete} found.\n")
+                    enterToContinue()
 
 
                 
-
+def enterToContinue():
+    input("Press enter to continue...")
 
 
 
